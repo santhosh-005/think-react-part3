0dc4a7b98f49da4b49816baacd8d636bdbd4c2db
santhosh-005 <santhoshdon000555@gmail.com>
2023-11-29 11:01:41 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Hello Kalvium</h1>
+      <h2>By Santhosh</h2>
+      <h3>From Sqaud <span>52</span></h3>
+
+      <p>It was done by react+vite</p>
+    </>
+  )
+}
+
+export default App
